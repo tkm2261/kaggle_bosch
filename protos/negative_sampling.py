3f81ad6b5a8045609ff9917fbb91ab95a47f1761
Takami Sato <takamisato@r.recruit.co.jp>
2016-09-08 21:25:10 +0900

    negative sam

--- protos/negative_sampling.py
+++ protos/negative_sampling.py
@@ -15,7 +15,7 @@ TRAIN_DATA = os.path.join(DATA_DIR, 'train_simple_join.csv.gz')
 TRAIN_POSITIVE_DATA = os.path.join(DATA_DIR, 'train_simple_join_pos.csv.gz')
 TEST_DATA = os.path.join(DATA_DIR, 'test_simple_join.csv.gz')
 TARGET_COLUMN_NAME = u'Response'
-#from feature import LIST_FEATURE_COLUMN_NAME
+from feature import LIST_FEATURE_COLUMN_NAME
 log_fmt = '%(asctime)s %(name)s %(lineno)d [%(levelname)s][%(funcName)s] %(message)s '
 logging.basicConfig(format=log_fmt,
                     datefmt='%Y-%m-%d/%H:%M:%S',
@@ -25,16 +25,21 @@ logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     logger.info('load start')
+
     train_pos_data = pandas.read_csv(TRAIN_POSITIVE_DATA)
-    train_pos_data = train_pos_data.fillna(0)
+    train_pos_data = train_pos_data.fillna(-1)
     list_train_data = pandas.read_csv(TRAIN_DATA, chunksize=train_pos_data.shape[0])
+
     logger.info('load end')
     logger.info('pos_data shape %s %s' % train_pos_data.shape)
 
-    feature_column = [col for col in train_pos_data.columns.values if col != TARGET_COLUMN_NAME and col != 'Id']
-    logger.info('feature_column %s' % len(feature_column))
     pos_target = train_pos_data[TARGET_COLUMN_NAME]
-    pos_data = train_pos_data[feature_column]
+    pos_data = train_pos_data[LIST_FEATURE_COLUMN_NAME]
+
+    start = 0
+    if 0:
+        pos_data = pandas.read_csv('pos_data_%s.csv.gz' % (start), index_col=0)
+        pos_target = pandas.read_csv('pos_target_%s.csv.gz' % (start), header=None, index_col=0)[1]
 
     params = {'subsample': 1, 'learning_rate': 0.1, 'colsample_bytree': 0.5,
               'max_depth': 5, 'min_child_weight': 0.01}
@@ -42,14 +47,22 @@ if __name__ == '__main__':
     model = None
 
     for i, train_data in enumerate(list_train_data):
-        train_data = train_data.fillna(0)
+        if i < start:
+            del train_data
+            gc.collect()
+            continue
+        # elif i == start:
+        #    model = LogisticRegression(n_jobs=-1)
+        #    model.fit(pos_data, pos_target)
+
+        train_data = train_data.fillna(-1)
 
         if i == 0:
             pos_target = pos_target.append(train_data[TARGET_COLUMN_NAME])
-            pos_data = pos_data.append(train_data[feature_column])
+            pos_data = pos_data.append(train_data[LIST_FEATURE_COLUMN_NAME])
         else:
             target = train_data[TARGET_COLUMN_NAME]
-            data = train_data[feature_column]
+            data = train_data[LIST_FEATURE_COLUMN_NAME]
             neg_target = target[target == 0]
             neg_data = data[target == 0]
             score = model.predict_proba(neg_data)[:, 1]
