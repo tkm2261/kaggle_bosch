32d52a852c485b444f8026d8a3b7242b2ec78d95
Ubuntu <ubuntu@ip-172-31-6-130.ap-northeast-1.compute.internal>
2016-10-01 17:47:07 +0000

    hash

--- protos/make_hash.py
+++ protos/make_hash.py
@@ -7,6 +7,7 @@ import numpy
 import glob
 import hashlib
 from multiprocessing import Pool
+from feature_orig import LIST_COLUMN_CAT, LIST_COLUMN_NUM, LIST_COLUMN_DATE
 
 APP_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../')
 DATA_DIR = os.path.join(APP_ROOT, 'data')
@@ -27,8 +28,13 @@ def read_csv(filename):
     logger.info(filename)
     ret = None
     for df in pandas.read_csv(filename, chunksize=10000):
+        df_ret = pandas.DataFrame()
         df = df[LIST_FEATURE_COLUMN_NAME]
-        df = df.apply(lambda row: hashlib.sha1((','.join(map(str, row))).encode('utf-8')).hexdigest(), axis=1)
+        df_ret['hash_all'] = df.apply(lambda row: hashlib.sha1((','.join(map(str, row))).encode('utf-8')).hexdigest(), axis=1)
+        df_ret['hash_cat'] = df[LIST_COLUMN_CAT].apply(lambda row: hashlib.sha1((','.join(map(str, row))).encode('utf-8')).hexdigest(), axis=1)
+        df_ret['hash_num'] = df[LIST_COLUMN_NUM].apply(lambda row: hashlib.sha1((','.join(map(str, row))).encode('utf-8')).hexdigest(), axis=1)
+        df_ret['hash_date'] = df[LIST_COLUMN_DATE].apply(lambda row: hashlib.sha1((','.join(map(str, row))).encode('utf-8')).hexdigest(), axis=1)
+
         if ret is None:
             ret = df
         else:
@@ -42,5 +48,5 @@ if __name__ == '__main__':
     list_path = sorted(list_path)
     p = Pool()    
     data = pandas.concat(p.map(read_csv, list_path)).reset_index(drop=True)
-    data = pandas.DataFrame(data, columns=['hash']).groupby('hash')['hash'].count()
+    #data = pandas.DataFrame(data, columns=['hash']).groupby('hash')['hash'].count()
     data.to_csv('../data/hash_table.csv')
